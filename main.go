e44ef48a21e1db3d31450a21c9202f56fc096eee
TudorOrban <130213626+TudorOrban@users.noreply.github.com>
2024-05-19 23:30:50 +0300

    Add first endpoint for API server

--- main.go
+++ main.go
@@ -1,6 +1,20 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+	"net/http"
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	r := mux.NewRouter()
+	r.HandleFunc("/", HomeHandler)
+	http.Handle("/", r)
+
+	fmt.Println("Server is running on http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the Maden API Server")
 }
