9430b3f8f9c5795317574b2c9a73fdfc2e6adedc
TudorOrban <130213626+TudorOrban@users.noreply.github.com>
2024-05-20 16:04:45 +0300

    Enhance scheduler with resource checks and affinity matching

--- scheduler.go
+++ scheduler.go
@@ -4,16 +4,40 @@ func schedulePod(pod *Pod) {
 	mu.Lock()
 	defer mu.Unlock()
 
-	// Round-robin scheduling
-	if len(nodeDB) > 0 {
-		for _, node := range nodeDB {
-			if node.Status == NodeReady {
-				pod.NodeID = node.ID
-				pod.Status = PodScheduled
-				return
-			}
+	for i, node := range nodeDB {
+		if node.Status == NodeReady && hasSufficentResources(&node, &pod.Resources) &&
+		   matchesAffinity(&node, pod) && matchesAntiAffinity(&node, pod) {
+			pod.NodeID = node.ID
+			pod.Status = PodScheduled
+			nodeDB[i].Used.CPU += pod.Resources.CPU
+			nodeDB[i].Used.Memory += pod.Resources.Memory
+			return
 		}
 	}
 
 	pod.Status = PodPending
+}
+
+func hasSufficentResources(node *Node, req *Resources) bool {
+	availableCPU := node.Capacity.CPU - node.Used.CPU
+	availableMemory := node.Capacity.Memory - node.Used.Memory
+	return availableCPU >= req.CPU && availableMemory >= req.Memory
+}
+
+func matchesAffinity(node *Node, pod *Pod) bool {
+	for key, val := range pod.Affinity {
+		if nodeVal, ok := node.Labels[key]; !ok || nodeVal != val {
+			return false
+		}
+	}
+	return true
+}
+
+func matchesAntiAffinity(node *Node, pod *Pod) bool {
+	for key, val := range pod.AntiAffinity {
+		if nodeVal, ok := node.Labels[key]; ok && nodeVal == val {
+			return false
+		}
+	}
+	return true
 }
--- types.go
+++ types.go
@@ -5,7 +5,9 @@ type Node struct {
 	ID string `json:"id"`
 	Name string `json:"name"`
 	Status NodeStatus `json:"status"`
-	Capacity NodeCapacity `json:"capacity"`
+	Capacity Resources `json:"capacity"`
+	Used Resources `json:"used"`
+	Labels map[string]string `json:"labels"`
 }
 
 type NodeCapacity struct {
@@ -19,4 +21,12 @@ type Pod struct {
 	Name string `json:"name"`
 	Status PodStatus `json:"status"`
 	NodeID string `json:"nodeId"`
+	Resources Resources `json:"resources"`
+	Affinity map[string]string `json:"affinity"`
+	AntiAffinity map[string]string `json:"antiAffinity"`
+}
+
+type Resources struct {
+	CPU int `json:"cpu"`
+	Memory int `json:"memory"` // in MB
 }
